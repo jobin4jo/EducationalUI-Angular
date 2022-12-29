3e431decf5f94c2db59fda19efd48937cb7c5d3b
Jobi S S <jobisjobi1234@gmail.com>
2022-12-29 22:52:48 +0530

    auth module some changes added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
+    SignupComponent,
     LoginComponent
   ],
   imports: [
